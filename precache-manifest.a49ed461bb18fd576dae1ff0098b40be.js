db5c31bc7d732c3191d2a81832e472d4494ba9c7
EuSet <setskoeugene@inbox.ru>
2021-03-31 11:41:47 +0300

    Updates

---
+++ precache-manifest.a49ed461bb18fd576dae1ff0098b40be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa747e3598ae54605c753468277ef219",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "d8d0213f6f72279fc21b",
+    "url": "/ToDoList/static/css/main.b7d2febd.chunk.css"
+  },
+  {
+    "revision": "eabedd4650c9ca1ff2c6",
+    "url": "/ToDoList/static/js/2.84c1fa4a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ToDoList/static/js/2.84c1fa4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8d0213f6f72279fc21b",
+    "url": "/ToDoList/static/js/main.ca9b9100.chunk.js"
+  },
+  {
+    "revision": "448f1ac8991118963dc3",
+    "url": "/ToDoList/static/js/runtime-main.97949b04.js"
+  }
+]);
